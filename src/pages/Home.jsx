d29baf1f2bc8465b75fb529da8c69c637e373bff
matheus philipe <omatheusphilipe@gmail.com>
2024-06-09 21:26:39 +0000

    feat adição da verificação da nota e do envio da nota para mudar de página

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,27 @@ import { CardContanier, IconContanier, ButtonContanier, ResultContanier } from "
 
 import iconStarImg from "../assets/icon-star.svg"
 import thankYouImg from "../assets/illustration-thank-you.svg"
+import { useState } from "react";
+
 
 export function Home() {
-  let aparecerResultado = true
+  const [mostrarResultado, setmostrarResultado] = useState(false)
+  const [feedbackNote, setfeedbackNote] = useState(0)
+
+  function handleFeedbackButtonClick(event){
+    const feedback = Number(event.target.innerText)
+
+    setfeedbackNote(feedback)
+  }
+
+  function handleSubmit() {
+    if (feedbackNote === 0)return
+
+    setmostrarResultado(true)
+  }
 
   return (
-    aparecerResultado === false ? (
+    mostrarResultado === false ? (
       <CardContanier>
       <IconContanier>
         <img src= {iconStarImg} alt="icone de estrela" />
@@ -17,21 +32,21 @@ export function Home() {
       <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
       
       <ButtonContanier>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
+        <button onClick={handleFeedbackButtonClick}>1</button>
+        <button onClick={handleFeedbackButtonClick}>2</button>
+        <button onClick={handleFeedbackButtonClick}>3</button>
+        <button onClick={handleFeedbackButtonClick}>4</button>
+        <button onClick={handleFeedbackButtonClick}>5</button>
       </ButtonContanier>
 
-      <button>Enviar</button>
+      <button onClick={handleSubmit}>Enviar</button>
     </CardContanier>
   ) : (
     <CardContanier>
       <img src={thankYouImg}  alt="imagem de agradecimento" />
     
       <ResultContanier>
-        <p>Você selecionou 4 de 5</p> 
+        <p>Você selecionou {feedbackNote} de 5</p> 
       </ResultContanier>
 
       <h1>Obrigado</h1>
