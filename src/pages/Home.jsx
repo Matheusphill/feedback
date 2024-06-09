1ff3d5f64709f03098f7ce6f4234cec1ad98bc33
matheus philipe <omatheusphilipe@gmail.com>
2024-06-09 20:18:30 +0000

    posicionamento do card no centro da terra

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { CardContanier, IconContanier } from "./Home.styles";
+
+import iconStarImg from "../assets/icon-star.svg"
 
 export function Home() {
   return (
-    <div>Home</div>
+    <CardContanier>
+      <IconContanier>
+        <img src= {iconStarImg} alt="icone de estrela" />
+      </IconContanier>
+
+      <h1>Como foi o atendimento</h1>
+      <p>Conte-nos como foi nossa ajuda com sua solicitação. Agradecemos muito seu feedback para podermos melhorar nosso atendimento!</p>
+      
+      <ButtonContanier>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+      </ButtonContanier>
+
+      <button>Enviar</button>
+    </CardContanier>
   )
 }
 
---
+++ src/pages/Home.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CardContanier = styled.div`
+    background: ${props => props.theme.cardGradient};
+    max-width: 41.2rem;
+    padding: 3.2rem;
+`
+
+export const IconContanier = styled.div`
+`
+
+export const ButtonContanier = styled.div`
+    
+`
